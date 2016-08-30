bb55d9c4544781d44c3025f1365f97205a39082c
Jeff R <ruhlio@users.noreply.github.com>
2016-08-29 21:40:25 -0500

    Added multipart parsing module

--- lib/athel/services/multipart.ex
+++ lib/athel/services/multipart.ex
@@ -17,8 +17,151 @@ defmodule Athel.Multipart do
     end
   end
 
-  @spec read_attachments(list(String.t), String.t) :: {:ok, attachment} | {:error, atom}
+  @spec read_attachments(list(String.t), String.t) :: {:ok, list(attachment)} | {:error, atom}
   def read_attachments(lines, boundary) do
+    attachments = lines |> parse(boundary) |> Enum.map(&cast_attachment/1)
+    {:ok, attachments}
+  catch
+    reason -> {:error, reason}
+  end
+
+  defp cast_attachment({headers, body}) do
+    type = get_type(headers)
+    filename = get_filename(headers)
+    body = decode_body(headers, body)
+
+    %{type: type, filename: filename, content: body}
+  end
+
+  defp get_type(headers) do
+    case headers["CONTENT-TYPE"] do
+      nil -> "text/plain"
+      #todo: handle charset param
+      {type, _params} -> type
+      type -> type
+    end
+  end
+
+  defp get_filename(headers) do
+    case headers["CONTENT-DISPOSITION"] do
+      nil ->
+        nil
+      {type, %{"filename" => filename}} when type in ["attachment", "form-data"] ->
+        filename
+      {_, _} ->
+        throw :unhandled_content_disposition
+      _ ->
+        throw :unhandled_content_disposition
+    end
+  end
+
+  defp decode_body(headers, body) do
+    case headers["CONTENT-TRANSFER-ENCODING"] do
+      nil ->
+        body
+      "base64" ->
+        case body do
+          [body] ->
+            case Base.decode64(body) do
+              {:ok, body} -> String.split(body, ~r/(\r\n)|\n|\r/)
+              :error -> throw :invalid_encoding
+            end
+          _ -> throw :invalid_encoding
+        end
+      _ ->
+        throw :unhandled_encoding
+    end
+  end
+
+  defp parse(lines, boundary) do
+    next_sigil = "--#{boundary}"
+    terminator = "--#{boundary}--"
+
+    # ignore comments (the top level body)
+    case parse_body(lines, terminator, next_sigil, []) do
+      {:continue, _, rest} ->
+        parse_attachments(rest, terminator, next_sigil, [])
+      {:terminate, _} ->
+        []
+    end
+  end
+
+  defp parse_body([], _, _, _) do
+    throw :unterminated_body
+  end
+
+  defp parse_body([line | _], terminator, _, acc) when line == terminator do
+    {:terminate, Enum.reverse(acc)}
+  end
+
+  defp parse_body([line | rest], _, next_sigil, acc) when line == next_sigil do
+    {:continue, Enum.reverse(acc), rest}
+  end
+
+  defp parse_body([line | rest], terminator, next_sigil,  acc) do
+    parse_body(rest, terminator, next_sigil, [line | acc])
+  end
+
+  defp parse_attachments([], _, _, _) do
+    throw :unterminated_body
+  end
+
+  defp parse_attachments(lines, terminator, next_sigil, attachments) do
+    case parse_attachment(lines, terminator, next_sigil) do
+      {:continue, attachment, rest} ->
+        parse_attachments(rest, terminator, next_sigil, [attachment | attachments])
+      {:terminate, attachment} ->
+        Enum.reverse([attachment | attachments])
+    end
+  end
+
+  defp parse_attachment(lines, terminator, next_sigil) do
+    {headers, rest} = parse_headers(lines, %{})
+    case parse_body(rest, terminator, next_sigil, []) do
+      {:continue, body, rest} ->
+        {:continue, {headers, body}, rest}
+      {:terminate, body} ->
+        {:terminate, {headers, body}}
+    end
+  end
+
+  defp parse_headers([], _) do
+    throw :unterminated_headers
+  end
+
+  defp parse_headers([line | rest], headers) when line == "" do
+    {headers, rest}
+  end
+
+  defp parse_headers([line | rest], headers) do
+    case Regex.run ~r/^([a-z0-9.-]+):\s*([^;]+)(;\s*\w+=".*")*/i, line do
+      nil ->
+        if Enum.empty?(headers) do
+          {%{}, [line | rest]}
+        else
+          throw :invalid_header
+        end
+      [_, name, value] ->
+        next_headers = Map.put(headers, name |> String.upcase, value |> String.trim)
+        parse_headers(rest, next_headers)
+      [_, name, value, raw_params] ->
+        params = parse_header_params(raw_params)
+        next_headers = Map.put(headers, name |> String.upcase, {value |> String.trim, params})
+        parse_headers(rest, next_headers)
+    end
+  end
+
+  # re-parses input, but regex can't return an array of matches from a subgroup
+  defp parse_header_params(raw_params) do
+    raw_params
+    |> String.split(";")
+    # skip first blank entry (String.split(";con;queso") == ["", "con", "queso"])
+    |> Stream.drop(1)
+    |> Enum.reduce(%{}, fn raw_param, params ->
+      # input already validated in parse_headers()
+      [_, name, value] = Regex.run ~r/^\s*(\w+)="(.*)"/, raw_param
+      Map.put(params, name, value)
+    end)
   end
 
 end
--- test/services/multipart_test.exs
+++ test/services/multipart_test.exs
@@ -22,46 +22,190 @@ defmodule Athel.MultipartTest do
   end
 
   test "no attachments" do
-    
+    assert read_attachments(
+      ["something",
+       "or",
+       "the",
+       "other",
+       "--trabajar--"],
+      "trabajar") == {:ok, []}
   end
 
   test "one attachment" do
+    attachment =
+      %{type: "text/notplain",
+        filename: "cristo.txt",
+        content: ["yo", "te", "llamo", "cristo", ""]}
+
+    assert read_attachments(
+      ["IGNORE",
+       "ME",
+       "--lapalabra",
+       "Content-Type: text/notplain",
+       "Content-Disposition: attachment ; filename=\"cristo.txt\"",
+       "",
+       "yo",
+       "te",
+       "llamo",
+       "cristo",
+       "",
+       "--lapalabra--"],
+      "lapalabra") == {:ok, [attachment]}
   end
 
   test "two attachments" do
-    
+    attachments =
+      [%{type: "text/notplain",
+         filename: "cristo.txt",
+         content: ["yo", "te", "llamo", "cristo"]},
+       %{type: "text/html",
+         filename: "my_homepage.html",
+         content: ["<h1>Cool things that I say to my friends</h1>", "Fire, walk with me"]}]
+
+    assert read_attachments(
+      ["IGNORE",
+       "ME",
+       "--lapalabra",
+       "Content-Type: text/notplain",
+       "Content-Disposition: attachment ; filename=\"cristo.txt\"",
+       "",
+       "yo",
+       "te",
+       "llamo",
+       "cristo",
+       "--lapalabra",
+       "Content-Type: text/html",
+       "Content-Disposition: attachment; filename=\"my_homepage.html\"",
+       "",
+       "<h1>Cool things that I say to my friends</h1>",
+       "Fire, walk with me",
+       "--lapalabra--"],
+      "lapalabra") == {:ok, attachments}
   end
 
-  test "no filename" do
-    
-  end
-
-  test "no content" do
-    
+  test "calls for help after the terminator are ignored" do
+    {:ok, [attachment]} = read_attachments(
+      ["--LUCHADOR",
+       "Content-Type: text/plain",
+       "",
+       "siempre",
+       "estas",
+       "aquí",
+       "--LUCHADOR--",
+       "ayúdame"],
+      "LUCHADOR")
+    assert attachment == %{type: "text/plain", filename: nil, content: ["siempre", "estas", "aquí"]}
   end
 
   test "base64 content" do
-    
+    {:ok, [attachment]} = read_attachments(
+      ["--planter",
+       "Content-Transfer-Encoding: base64",
+       "",
+       "Q2FuJ3QgZ2V0IG15DQpsaW5lIGVuZGluZ3MKY29uc2lzdGVudA1pIHF1aXQ=",
+       "--planter--"],
+      "planter")
+    assert attachment.content == ["Can't get my", "line endings", "consistent", "i quit"]
+  end
+
+  test "attachment without headers" do
+    {:ok, [attachment]} = read_attachments(
+      ["--box",
+       "just a body",
+       "kinda shoddy",
+       "--box--"],
+      "box")
+    assert attachment == %{type: "text/plain", filename: nil, content: ["just a body", "kinda shoddy"]}
   end
 
   test "missing terminator with no attachments" do
-    
+    assert read_attachments(
+      ["just",
+       "can't",
+       "stop",
+       "myself"],
+      "FASTLANE") == {:error, :unterminated_body}
   end
 
   test "missing terminator with an attachment" do
-    
+    assert read_attachments(
+      ["la gloria",
+       "de Dios",
+       "--amigo",
+       "Content-Type: text/plain",
+       "",
+       "get at me",
+       "--amigo"],
+      "amigo") == {:error, :unterminated_body}
+
+    assert read_attachments(
+      ["--amigo",
+       "Content-Type: text/plain",
+       "",
+       "sacrebleu!"],
+      "amigo") == {:error, :unterminated_body}
   end
 
   test "missing newline after headers" do
-    
+    assert read_attachments(
+      ["--lastsupper",
+       "Content-Type: bread/wine",
+       "this is my body",
+       "--lastsupper--"],
+      "lastsupper") == {:error, :invalid_header}
+
+    assert read_attachments(
+      ["--lastsupper",
+       "Content-Type: bread/wine"],
+      "lastsupper") == {:error, :unterminated_headers}
   end
 
-  test "invalid encoding type" do
-    
+  test "unhandled encoding type" do
+    assert read_attachments(
+      ["--toots",
+       "Content-Transfer-Encoding: base58",
+       "",
+       "--toots--"],
+      "toots") == {:error, :unhandled_encoding}
   end
 
   test "invalid encoding" do
-    
+    assert read_attachments(
+      ["--boots",
+       "Content-Transfer-Encoding: base64",
+       "",
+       "can't b64",
+       "two lines",
+       "--boots--"],
+      "boots") == {:error, :invalid_encoding}
+
+    assert read_attachments(
+      ["--boots",
+       "Content-Transfer-Encoding: base64",
+       "",
+       "whoopsy",
+       "--boots--"],
+      "boots") == {:error, :invalid_encoding}
+  end
+
+  test "unhandled content disposition" do
+    assert read_attachments(
+      ["--butts",
+       "Content-Disposition: inline; filename=\"nice_heels.jpg\"",
+       "",
+       "sometimes frosted",
+       "sometimes sprinkled",
+       "--butts--"],
+      "butts") == {:error, :unhandled_content_disposition}
+
+    assert read_attachments(
+      ["--butts",
+       "Content-Disposition: inline",
+       "",
+       "sometimes frosted",
+       "sometimes sprinkled",
+       "--butts--"],
+      "butts") == {:error, :unhandled_content_disposition}
   end
 
 end
