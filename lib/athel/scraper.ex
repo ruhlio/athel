7b3317df7b3d0a2cf5007d0e320690e0c9876937
Jeff R <ruhlio@users.noreply.github.com>
2019-01-17 21:04:03 -0600

    Beefed up scraper
    Scraper now initializes asynchronously using `GenServer`'s continue
    and fetches by group using `Task.async_stream`. Individuals article
    fetches are also now more fault tolerant, skipping an unparsable record
    and resetting the connection to continue with the rest of the articles.

--- lib/athel/scraper.ex
+++ lib/athel/scraper.ex
@@ -5,32 +5,41 @@ defmodule Athel.Scraper do
 
   alias Athel.{Nntp, NntpService, Repo, Article}
 
-  def start_link(foreigner) do
+  def start_link(foreigner = %Athel.Foreigner{}) do
     GenServer.start_link(__MODULE__, foreigner, [])
   end
 
   @impl true
   def init(foreigner) do
-    {:ok, session} = Nntp.Client.connect(foreigner.hostname, foreigner.port)
+    {:ok, %{foreigner: foreigner, groups: [], message_id_index: nil}, {:continue, :initialize_session}}
+  end
+
+  @impl true
+  def handle_continue(:initialize_session, state = %{foreigner: foreigner}) do
+    {:ok, session} = connect(foreigner)
     groups = find_groups(session)
     if Enum.empty?(groups) do
+      Nntp.Client.quit(session)
       Logger.warn "No common groups found at #{foreigner.hostname}:#{foreigner.port}"
-      :ignore
+      {:noreply, state}
     else
       message_id_index = find_message_id_index(session)
       Nntp.Client.quit(session)
       Logger.info "Found common groups #{inspect groups} at #{foreigner.hostname}:#{foreigner.port}"
       Process.send_after(self(), :run, 0)
 
-      {:ok, %{groups: groups, message_id_index: message_id_index, foreigner: foreigner}}
+      {:noreply, %{state | groups: groups, message_id_index: message_id_index}}
     end
   end
 
   defp find_groups(session) do
     local_groups = Athel.Group |> Athel.Repo.all |> Enum.map(&(&1.name)) |> MapSet.new
     {:ok, foreign_group_resp} = Nntp.Client.list(session)
-    foreign_groups = MapSet.new Enum.map(foreign_group_resp, fn line -> line |> String.split |> List.first end)
-    MapSet.intersection(local_groups, foreign_groups)
+    foreign_groups = foreign_group_resp
+    |> Enum.map(fn line -> line |> String.split |> List.first end)
+    |> MapSet.new()
+
+    MapSet.intersection(local_groups, foreign_groups) |> MapSet.to_list()
   end
 
   defp find_message_id_index(session) do
@@ -40,27 +49,49 @@ defmodule Athel.Scraper do
   end
 
   @impl true
-  def handle_info(:run, state) do
-    {:ok, session} = Nntp.Client.connect(state.foreigner.hostname, state.foreigner.port)
-    Enum.each state.groups, fn group -> scrape_articles(state, session, group) end
+  def handle_info(:run, state = %{foreigner: foreigner,
+                                  groups: groups,
+                                  message_id_index: message_id_index}) do
+    stream = Task.async_stream(groups, Athel.Scraper, :scrape_group, [foreigner, message_id_index],
+      timeout: 120_000, on_timeout: :kill_task, ordered: false)
+    Stream.run(stream)
 
-    # Process.send_after(self(), :run, state.foreigner.interval)
+    #Process.send_after(self(), :run, state.foreigner.interval + timeout)
+    {:noreply, state}
   end
 
-  defp scrape_articles(state, session, group) do
+  @impl true
+  def handle_info(type, _state) do
+    Logger.info "Received: #{inspect type}"
+  end
+
+  def scrape_group(group, foreigner, message_id_index) do
+    {:ok, session} = connect(foreigner)
     :ok = Nntp.Client.set_group(session, group)
     {:ok, overviews} = Nntp.Client.xover(session, 1)
-    ids = extract_ids(overviews, state.message_id_index)
-    id_set = MapSet.new ids
+
+    ids = extract_ids(overviews, message_id_index)
+    id_set = MapSet.new(ids)
     existing_ids = Repo.all(
       from article in Article,
       select: article.message_id) |> MapSet.new
-    new_ids = MapSet.difference(id_set, existing_ids)
-    new_id_count = MapSet.size(new_ids)
-    if new_id_count > 0 do
-      Logger.info "Found #{new_id_count} new messages for group #{group}"
-      Enum.each new_ids, fn id -> fetch_id(session, id) end
+    new_ids = MapSet.difference(id_set, existing_ids) |> MapSet.to_list()
+    Logger.info "Found #{length new_ids} new messages for group #{group}"
+    fetch_ids(new_ids, session, group, foreigner)
+  end
+
+  defp fetch_ids([id | rest], session, group, foreigner) do
+    case fetch_id(session, id) do
+      true -> fetch_ids(rest, session, group, foreigner)
+      false ->
+        Nntp.Client.quit(session)
+        {:ok, new_session} = connect(foreigner)
+        :ok = Nntp.Client.set_group(new_session, group)
+        fetch_ids(rest, new_session, group, foreigner)
     end
+  end
+  defp fetch_ids([], session, _, _) do
+    Nntp.Client.quit(session)
   end
 
   defp extract_ids(overviews, message_id_index) do
@@ -86,13 +117,18 @@ defmodule Athel.Scraper do
           _ ->
             Logger.info "Took #{id}"
         end
+        true
       {:error, {code, message}} when is_number(code) ->
         Logger.warn "Server rejected request for #{id}: #{message}"
+        true
       {:error, reason} ->
         # Failed parse, can't recover
         Logger.error "Failed to parse #{id}: #{inspect reason}"
-        Nntp.Client.quit(session)
-        throw {:stop, :bad_parse}
+        false
     end
+  end
+
+  defp connect(foreigner) do
+    Nntp.Client.connect(foreigner.hostname, foreigner.port)
   end
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Athel.Mixfile do
   def project do
     [app: :athel,
      version: "0.1.0",
-     elixir: "~> 1.7",
+     elixir: "~> 1.8",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
